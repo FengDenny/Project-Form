7102c17b05675fbb1187ea395e5f627198a5c40b
FengDenny <fengdennyy@gmail.com>
2021-11-22 20:12:42 -0800

    Need to use hashrouter for ghpgaes deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+// need to use hashrouter for ghpages deployment
 import { HashRouter as Router, Route } from "react-router-dom";
 import { GlobalStyles } from "./styled-components/globalStyled";
 import { ModalProvider } from "styled-react-modal";
